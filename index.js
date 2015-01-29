5becc3f381a9a8c2412ec635def58f98344037d0
Jean-Christophe Hoelt <hoelt@fovea.cc>
2015-01-29 18:49:53 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,46 @@
+var bouncy = require('bouncy');
+var url = require("url");
+
+if (process.argv.length < 4) {
+    console.log("usage: api-server <apiRoot> <apiHost> <apiPort>");
+    process.exit(1);
+}
+
+var apiRoot = process.argv[2];
+var apiHost = process.argv[3];
+var apiPort = process.argv[4] || 80;
+
+var httpServer = require("http-server");
+httpServer.createServer({
+    root: "./"
+}).listen(8080);
+
+var server = bouncy(function (req, res, bounce) {
+    var uri = url.parse(req.url).pathname;
+    var uriArray = uri.split("/");
+    console.log(uri);
+    if (uriArray.length > 2) {
+        var s = uriArray[1];
+        if (s === apiRoot) {
+            console.log("bouncing to " + apiHost + ":" + apiPort);
+            bounce(apiHost, apiPort, {
+                headers: {
+                    Connection: "close"
+                }
+            });
+        }
+        else {
+            console.log("serving locally");
+            bounce("localhost", 8080, {
+                headers: {
+                    Connection: "close"
+                }
+            });
+        }
+        return;
+    }
+    res.statusCode = 404;
+    res.end('no such host');
+});
+server.listen(8000);
+
